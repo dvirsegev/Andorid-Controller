2f8dca6d5b195f7336047378875ac5d8dbfe6efe
Dvir Segev <dvirsegev@gmail.com>
2019-06-17 16:54:58 +0300

    finish + landscape

--- app/src/main/java/com/example/ex2/Tcp.java
+++ app/src/main/java/com/example/ex2/Tcp.java
@@ -17,13 +17,14 @@ public class Tcp  extends AsyncTask<String, String, Void>  {
 
     @Override
     protected Void doInBackground(String... strings) {
-        try {
-
-            Socket socket = new Socket(strings[0], Integer.parseInt(strings[1]));
-            DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
-            try {
-                writer.write((queue.take() + "\r\n").getBytes());
-                writer.flush();
+                    try {
+                        Socket socket = new Socket(strings[0], Integer.parseInt(strings[1]));
+                        DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
+                        try {
+                            while(true) {
+                                writer.write((queue.take() + "\r\n").getBytes());
+                                writer.flush();
+                            }
             } catch (IOException e) {
                 Log.e("TCP", "S: Error", e);
             } finally {
